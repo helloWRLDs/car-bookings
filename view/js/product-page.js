78695737c5c2fcdad6a6edbf9691f43ad66f384e
Danil Li <danil.li24x@gmail.com>
2024-04-19 14:01:11 +0500

    front

--- view/js/product-page.js
+++ view/js/product-page.js
@@ -1,16 +1,22 @@
 const btn = document.getElementById("searchBtn")
+const paginationPanel = document.getElementById('paginationPanel')
 const url = 'http://localhost:9090/api/v1/cars?'
 let currentUrl = url
-const limit = 13
+const limit = 8
 let offset = 0
+let currentPage = 1
+let color = ""
+let model = ""
+let order = "asc"
+let sort = "id"
 
-btn.addEventListener("click", async(e) => {
-    e.preventDefault()
+const loadContent = async() => {
     let request = url
-    const color = document.getElementById("color").value
-    const model = document.getElementById("filter").value.trim()
-    request += `color=${color}&model=${model}&limit=${limit}&offset=${offset}`;
-    console.log(url)
+    color = document.getElementById("color").value
+    model = document.getElementById("filter").value.trim()
+    order = document.getElementById('order').value
+    sort = document.getElementById('sort').value
+    request += `color=${color}&model=${model}&limit=${limit}&offset=${offset}&sort=${sort}&order=${order}`;
 
     try {
         let response = await fetch(request, {
@@ -25,7 +31,7 @@ btn.addEventListener("click", async(e) => {
         offers.innerHTML = '';
         data.content.forEach(elem => {
             const div = document.createElement('div');
-            div.classList.add('grid', 'items', 'bg-green-700', 'px-5', 'py-2', 'relative');
+            div.classList.add('grid', 'items', 'bg-slate-700', 'text-white', 'px-5', 'py-2', 'relative');
             div.innerHTML = `
                 <h5 class="text-xl">${elem.vendor} ${elem.model}</h5>
                 <p class="text-sm">Year: <span>${elem.year}</span></p>
@@ -37,10 +43,42 @@ btn.addEventListener("click", async(e) => {
             `;
             offers.appendChild(div);
         });
-        currentUrl = request.slice(0, request.length - 17)
-        offset = 0
-        console.log("current url: ", currentUrl)
     } catch(error) {
         console.error(error)
     }
+}
+
+btn.addEventListener("click", async(e) => {
+    e.preventDefault()
+    loadContent()
 })
+
+const loadPaginationPanel = async() => {
+    paginationPanel.innerHTML = ""
+    for (i = 0; i < 5; i++) {
+        const btn = document.createElement('button')
+        btn.classList.add("px-3", "py-1", "pg-btn")
+        if (offset === i * limit) {
+            btn.classList.add("bg-blue-500", "text-white")
+            btn.disabled = true
+        } else {
+            btn.classList.add("bg-slate-300", "text-gray-700", "hover:bg-slate-400", "hover:text-white")
+        }
+        btn.value = i * limit
+        btn.innerHTML = i + 1
+        paginationPanel.appendChild(btn)
+        btn.addEventListener("click", (e) => {
+            e.preventDefault()
+            btn.disabled = true
+            offset = btn.value
+            loadContent()
+            loadPaginationPanel()
+        })
+    }
+}
+
+
+window.onload = () => {
+    loadPaginationPanel()
+    loadContent()
+}
